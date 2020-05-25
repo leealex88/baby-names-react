a637730f9f067b893d4e6c08f81d10f19cc6be9f
ola <olasvoice@gmail.com>
2020-05-25 18:02:35 +0100

    GenderButtons component added

---
+++ src/components/GenderButtons.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function GenderButtons({ setSelectedGender }) {
+  return (
+    <div className="buttons-container">
+      <input
+        type="radio"
+        value="all"
+        name="gender"
+        onChange={(e) => setSelectedGender(e.target.value)}
+      />{" "}
+      All
+      <input
+        type="radio"
+        value="m"
+        name="gender"
+        onChange={(e) => setSelectedGender(e.target.value)}
+      />{" "}
+      Male
+      <input
+        type="radio"
+        value="f"
+        name="gender"
+        onChange={(e) => setSelectedGender(e.target.value)}
+      />{" "}
+      Female
+    </div>
+  );
+}
+
+export default GenderButtons;
